17b6a14aa4018c611e120c334e2e71463eea375f
alexhonda <alex.t.honda@gmail.com>
2015-12-08 19:45:15 -0200

    initial commit

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+public float speed = 0;
+public float switchTime = 2;
+
+	
+void Start () {
+
+	GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+
+	}
+	
+
+	void Update () {
+	
+	}
+}
